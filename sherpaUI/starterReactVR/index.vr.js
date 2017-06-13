3d1d2c68df688824d6272e509ef87ab4feab928c
Alyssa Gaudioso <alyssa.gaudioso@gmail.com>
2017-06-13 16:37:47 -0700

    added ReactVR index and frame components and gitignore

--- sherpaUI/starterReactVR/index.vr.js
+++ sherpaUI/starterReactVR/index.vr.js
@@ -1,34 +1,55 @@
-import React from 'react';
-import {
-  AppRegistry,
-  asset,
-  Pano,
-  Text,
-  View,
-} from 'react-vr';
+import React, { Component } from 'react';
+import { AppRegistry, asset, Pano, View, Text, StyleSheet, Plane, Scene } from 'react-vr';
+import Frame from './frame.vr.js';
+import data from './obj.js';
 
-export default class starterReactVR extends React.Component {
+const width = 10;
+
+export default class Basics extends Component {
+  constructor() {
+    super();
+    this.state = data;
+  }
+
   render() {
     return (
-      <View>
-        <Pano source={asset('chess-world.jpg')}/>
-        <Text
-          style={{
-            backgroundColor: '#777879',
-            fontSize: 0.8,
-            fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
-          }}>
-          hello
-        </Text>
-      </View>
-    );
+      // <Scene>
+        <View style={styles.container}>
+          <Pano source={asset(this.state.image)}></Pano>
+          <Plane
+            text={'idk this is text'}
+            style={{color: 'black',
+                    transform: [{translate: [-width/2, 0, -3]}]
+                  }}
+            dimWidth={1}
+            dimHeight={1}
+          />
+          <Plane
+            text={'idk this is text'}
+            style={{color: 'blue',
+                    transform: [{translate: [-width/2, 0, -3]}]
+                  }}
+            dimWidth={1}
+            dimHeight={1}
+            wireFrame={true}
+          />
+          <Frame text={this.state.front.text} translate={[-width/2, 0, -3]} rotateY={0}/>
+          <Frame text={this.state.right.text} translate={[-width/2, .125, -3]} rotateY={-90}/> 
+          <Frame text={this.state.back.text} translate={[-width/2, .25, -3]} rotateY={180}/>
+          <Frame text={this.state.left.text} translate={[-width/2, .375, -3]} rotateY={90}/>
+        </View>
+      // </Scene>
+    )
   }
 };
 
-AppRegistry.registerComponent('starterReactVR', () => starterReactVR);
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: width,
+    alignItems: 'center',
+    flexDirection: 'column',
+  }
+})
+
+AppRegistry.registerComponent( 'Basics', () => Basics );
