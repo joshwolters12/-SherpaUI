32613146472918ce94b4b40a0fe9d19596e45dbd
Alyssa Gaudioso <alyssa.gaudioso@gmail.com>
2017-06-15 19:51:17 -0700

    starting to get nav....

--- sherpaUI/starterReactVR/frame.vr.js
+++ sherpaUI/starterReactVR/frame.vr.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { AppRegistry, asset, Pano, View, Text, StyleSheet } from 'react-vr';
+import { AppRegistry, asset, Pano, View, Text, StyleSheet, VrButton } from 'react-vr';
 import data from './obj.js';
 
 export default class Frame extends Component {
@@ -7,11 +7,30 @@ export default class Frame extends Component {
   render() {
     return (
       <View style={{}}>
+
+        {/*<VrButton 
+          style={{
+            transform: [ {translate: [0,0,-5]}], 
+          }}
+          onClick={() => this.navigate()}>
+          <Text>move to the left</Text>
+        </VrButton>*/}
+
         <Text style={{
                 transform: [ {translate: this.props.translate}, {rotateY: this.props.rotateY}], 
                 fontSize: .2,
-              }}
-        >{this.props.text}</Text>
+              }}>
+              {this.props.text}
+        </Text>
+
+        {/*<VrButton 
+          style={{
+            transform: [ {translate: [0,0,-5]}], 
+          }}
+          onClick={() => this.navigate()}>
+          <Text>move to the right</Text>
+        </VrButton>*/}
+
       </View>
     )
   }
