abe4870042629039db132f7d80b161b53991c552
Aaron Goldsmith <aarongoldsmith@msn.com>
2017-06-16 15:18:14 -0700

    add electron dist scripts, dependencies

--- sherpaUI/main.js
+++ sherpaUI/main.js
@@ -1,14 +1,13 @@
-import { app, BrowserWindow, dialog, Menu, shell } from 'electron';
-// const dialog = require('electron').remote
+const app = require('electron').app;
+const BrowserWindow = require('electron').BrowserWindow;
+const dialog = require('electron').dialog;
+const Menu = require('electron').Menu;
+const shell = require('electron').shell;
 const exec = require('child_process').exec;
 const fs = require('fs-extra');
 const defaultMenu = require('electron-default-menu');
 
-
-
-
 let mainWindow = null;
-
 
 app.on('window-all-closed', () => {
   if (process.platform != 'darwin') {
@@ -38,51 +37,18 @@ app.on('ready', () => {
     ]
   })
 
-
-
-
-
-
-
   exec('node starterReactVR/node_modules/react-native/local-cli/cli.js start')
   mainWindow = new BrowserWindow({
     width: 1200,
     height: 800
   });
+
   setTimeout(function() {
-    mainWindow.loadURL('file://' + __dirname + '/index.html',);
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
   }, 500)
 
-  //LOAD A FILE TO THE STATIC ASSETS DIRECTORY
-  // dialog.showOpenDialog({
-  //   filters: [
-  //     {
-  //       name: 'Images',
-  //       extensions: ['jpg', 'png', 'gif']
-  //     }
-  //   ]
-  // }, function(filePath) {
-  //   if (filePath === undefined) return;
-  //   let imageToLoad = filePath[0].split("/").pop()
-  //   fs.copy(filePath.toString(), 'starterReactVR/static_assets/' + imageToLoad, function(err) {
-  //     if (err) return console.log(err)
-  //   })
-  //   fs.readFile('starterReactVR/myjsonfile.json', 'utf8', function(err, data) {
-  //     let obj = JSON.parse(data)
-  //     obj.imageURL = imageToLoad
-  //     let json = JSON.stringify(obj, null, 2)
-  //     fs.writeFile('./starterReactVR/myjsonfile.json', json, 'utf8', function(err) {
-  //       if (err) return console.log(err)
-  //       mainWindow.reload()
-  //     })
-  //   })
-  // });
-
-
-
-
-
   Menu.setApplicationMenu(Menu.buildFromTemplate(menu))
+
   mainWindow.on('closed', () => {
     mainWindow = null;
   });
