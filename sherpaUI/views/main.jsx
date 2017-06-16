518bef22bf1ac612e05d95a1b67bfa1558ef9e56
Aaron Goldsmith <aarongoldsmith@msn.com>
2017-06-15 19:22:55 -0700

    exporting project to desktop

--- sherpaUI/views/main.jsx
+++ sherpaUI/views/main.jsx
@@ -3,7 +3,9 @@ import { SegmentedControl, SegmentedControlItem, Text } from 'react-desktop/macO
 import Gui from '../components/Gui';
 const fs = require('fs-extra');
 var data = require('../starterReactVR/myjsonfile.json');
-const dialog = require('electron').remote.dialog;
+import { BrowserWindow, dialog } from 'electron';
+
+console.log(dialog)
 
 export default class Main extends Component {
   constructor() {
@@ -38,60 +40,56 @@ export default class Main extends Component {
   }
 
   chooseImage() {
-    let _this = this;
-    new Promise((resolve, reject) => {
-      dialog.showOpenDialog({
-        filters: [
-          {
-            name: 'Images',
-            extensions: ['jpg', 'png', 'gif']
-          }
-        ]
-      }, function (filePath) {
-        if (filePath === undefined) return;
-        let imageToLoad = filePath[0].split("/").pop();
-        let pathLength = filePath[0].split("/").length;
-        let pathMatch = filePath[0].split("/").slice(pathLength - 3,pathLength).join("/");
-
-        if (pathMatch !== 'starterReactVR/static_assets/' + imageToLoad) {
-          console.log('filePath', filePath)
-          console.log('saveURI', 'starterReactVR/static_assets/' + imageToLoad)
-          fs.copy(filePath.toString(), 'starterReactVR/static_assets/' + imageToLoad, function (err) {
-            if (err) return console.log(err)
-            resolve(imageToLoad)
-          })
-        } else {
-          resolve(imageToLoad)
+    console.log('choosing image')
+    dialog.showOpenDialog({
+      filters: [
+        {
+          name: 'Images',
+          extensions: ['jpg', 'png', 'gif']
         }
+      ]
+    }, function (filePath) {
+      if (filePath === undefined) return;
+      let imageToLoad = filePath[0].split("/").pop()
+
+      fs.copy(filePath.toString(), 'starterReactVR/static_assets/' + imageToLoad, function (err) {
+        if (err) return console.log(err)
       })
-    }).then((imageURL) => {
-      let newState = _this.state;
-      newState.imageURL = imageURL
-      this.setState(newState)
-      this.writeToFile()
+
+      fs.readFile('starterReactVR/myjsonfile.json', 'utf8', function (err, data) {
+        let obj = JSON.parse(data)
+        obj.imageURL = imageToLoad
+        let json = JSON.stringify(obj, null, 2)
+
+        fs.writeFile('./starterReactVR/myjsonfile.json', json, 'utf8', function (err) {
+          if (err) return console.log(err)
+          mainWindow.reload()
+        })
+
+      })
     })
   }
 
   render() {
-    return (
-      <div id='appcontainer' style={styles.appcontainer} >
-        <div id="headspacer" style={styles.header}>
-          <div style={styles.logo}>
-            <img src="./starterReactVR/static_assets/sherpa.png" />
-          </div>
+        return(
+      <div id= 'appcontainer' style= { styles.appcontainer } >
+      <div id="headspacer" style={styles.header}>
+        <div style={styles.logo}>
+          <img src="./starterReactVR/static_assets/sherpa.png" />
         </div>
-        <Gui
-          data={this.state}
-          selectPage={this.selectPage}
-          updateProperties={this.updateProperties}
-          writeToFile={this.writeToFile}
-          loadURL={this.state.loadURL}
-          imageURL={this.state.imageURL}
-          chooseImage={this.chooseImage}
-        ></Gui>
-        <div id="footer" style={styles.footer}></div>
+      </div>
+      <Gui
+        data={this.state}
+        selectPage={this.selectPage}
+        updateProperties={this.updateProperties}
+        writeToFile={this.writeToFile}
+        loadURL={this.state.loadURL}
+        imageURL={this.state.imageURL}
+        chooseImage = {this.chooseImage}
+      ></Gui>
+      <div id="footer" style={styles.footer}></div>
       </div >
-    );
+      );
   }
 }
 
@@ -115,8 +113,8 @@ let styles = {
     flex: '[1 0 10%]',
   },
   logo: {
-    width: '150px',
-    height: '35px',
+    width: '200px',
+    height: '48px',
     margin: 'auto',
     alignItems: 'center'
   }
