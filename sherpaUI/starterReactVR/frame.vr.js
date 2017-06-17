21006ff58304df85ec9f7fc84a124f54dd1b0d6e
Alyssa Gaudioso <alyssa.gaudioso@gmail.com>
2017-06-16 20:31:14 -0700

    click panels to update view

--- sherpaUI/starterReactVR/frame.vr.js
+++ sherpaUI/starterReactVR/frame.vr.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { AppRegistry, VrButton, NativeModules, asset, Pano, View, Text, StyleSheet, Scene, VrHeadModel } from 'react-vr';
+import { AppRegistry, VrButton, NativeModules, asset, Pano, View, Text, StyleSheet, Scene, VrHeadModel, Image } from 'react-vr';
 import data from './obj.js';
 
 export default class Frame extends Component {
@@ -11,27 +11,14 @@ export default class Frame extends Component {
     return (
       <View>
 
-        {/*NAV LEFT*/}
-        <VrButton 
-          style={{
-            transform: [ {translate: this.props.translate}, {rotateY: this.props.rotateY}],
-            flexDirection: 'row', 
-          }}
-          onClick={() => this.props.navigate(this.props.rotateY, 1)}>
-          <Text>move to the left</Text>
-        </VrButton>
-        {/*NAV LEFT*/}
-
-        {/*NAV RIGHT*/}
-        <VrButton 
-          style={{
-            transform: [ {translate: this.props.translate}, {rotateY: this.props.rotateY}],
-            flexDirection: 'row', 
-          }}
-          onClick={() => this.props.navigate(this.props.rotateY, -1)}>
-          <Text>move to the right</Text>
-        </VrButton>
-        {/*NAV RIGHT*/}
+        {/*TITLE*/}
+        <Text style={{
+                transform: [ {translate: this.props.translate}, {rotateY: this.props.rotateY}], 
+                fontSize: .5,
+              }}>
+              {this.props.title}
+        </Text>
+        {/*TITLE*/}
 
         {/*TEXT*/}
         <Text style={{
@@ -41,6 +28,36 @@ export default class Frame extends Component {
               {this.props.text}
         </Text>
         {/*TEXT*/}
+
+        <View>
+          {/*NAV LEFT*/}
+          <VrButton 
+            style={{
+              transform: [ {translate: this.props.translate}, {rotateY: this.props.rotateY}],
+              flexDirection: 'row', 
+            }}
+            onClick={() => this.props.navigate(this.props.rotateY, 1)}>
+            <Text style={{
+                fontSize: .15,
+              }}>move to the left</Text>
+              <Image source={asset('arrowleft.png')}
+                     style={{width: 1, height: 1}} />
+          </VrButton>
+          {/*NAV LEFT*/}
+
+          {/*NAV RIGHT*/}
+          <VrButton 
+            style={{
+              transform: [ {translate: this.props.translate}, {rotateY: this.props.rotateY}],
+              flexDirection: 'row', 
+            }}
+            onClick={() => this.props.navigate(this.props.rotateY, -1)}>
+            <Text style={{
+                    fontSize: .15,
+                  }}>move to the right</Text>
+          </VrButton>
+          {/*NAV RIGHT*/}
+        </View>
 
       </View>
     )
